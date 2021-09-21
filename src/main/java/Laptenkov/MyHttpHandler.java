4bd3e2c12754ae0ef673b308c2d69638320ee6a3
habatoo <habatoo@gmail.com>
2021-09-21 11:45:59 +0700

    - MyHttpHandler

--- src/main/java/Laptenkov/MyHttpHandler.java
+++ src/main/java/Laptenkov/MyHttpHandler.java
@@ -18,18 +18,40 @@ public class MyHttpHandler implements HttpHandler {
         System.out.println(httpExchange.getRequestHeaders().entrySet());
 
         OutputStream outputStream = httpExchange.getResponseBody();
+        String requestURIMethod = httpExchange.getRequestMethod();
+        String requestURI = httpExchange.getRequestURI().toString();
+        int rCode;
+
         StringBuilder htmlBuilder = new StringBuilder();
         htmlBuilder.append("<html>")
                 .append("<body>")
                 .append("<h1>")
                 .append("Method: " + httpExchange.getRequestMethod())
                 .append(" on uri: " + httpExchange.getRequestURI())
-                .append("</h1>")
+                .append("</h1>");
+
+        if (requestURIMethod.equals("GET") &&
+                requestURI.equals("/about")) {
+            htmlBuilder
+                    .append("<h2>My web page</h2>")
+                    .append("<hr>")
+                    .append("<p>About me:")
+                    .append("<p>author: habatoo")
+                    .append("<p>company: Sber")
+                    .append("<p>position: audit")
+                    .append("<hr>");
+            rCode = 200;
+        } else {
+            htmlBuilder
+                    .append("<h1>Error, page not found.</h1>");
+            rCode = 404;
+        }
+
+        htmlBuilder
                 .append("</body>")
                 .append("</html>");
-
         String htmlResponse = htmlBuilder.toString();
-        httpExchange.sendResponseHeaders(200, htmlResponse.length());
+        httpExchange.sendResponseHeaders(rCode, htmlResponse.length());
         outputStream.write(htmlResponse.getBytes());
         outputStream.flush();
         outputStream.close();
